59a695e39363f7bf10b45f77c288ee4168c49193
evelin-code <f.gongoraandrade@gmail.com>
2024-08-11 15:18:30 -0500

    Config cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from 'cors';
 import employeeRoutes from './routes/employee.routes';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
@@ -7,8 +8,18 @@ import requestRoutes from './routes/request.routes';
 
 const app = express();
 
+// Habilitar CORS primero
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+}));
+
+// Middleware
 app.use(morgan('dev'));
 app.use(express.json()); 
+
+// Routes
 app.use('/api/employees', employeeRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import cors from 'cors';
 import app from './app';
 import './databaseSync';
 
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
