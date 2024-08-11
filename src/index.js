93969308f214fb5c15cf8bafd084c9a4f447ee25
evelin-code <f.gongoraandrade@gmail.com>
2024-08-11 13:40:09 -0500

    Config cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import './databaseSync';
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
