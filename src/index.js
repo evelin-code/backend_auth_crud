893aa4c39a4f209516b25d29d2480a8d55a9cac3
evelin-code <f.gongoraandrade@gmail.com>
2024-08-11 14:56:09 -0500

    Config cors

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: false
 }));
+
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
