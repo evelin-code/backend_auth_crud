f106ea2aada11ea72d76fcdc19ab123b85544296
evelin-code <f.gongoraandrade@gmail.com>
2024-08-11 14:45:16 -0500

    Config cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  credentials: false
 }));
 
 app.listen(PORT, () => {
